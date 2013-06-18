fe6b5015420ab5c3487d9bfc72f0b6f9f963b436
zhangxl0331 <zhangxl0331@gmail.com>
2013-06-18 16:04:28 +0800

    modify

--- application/language/english/cp_lang.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$lang = array(
-
-	'formhash' => '表单验证串',
-);
--- application/modules/doing/views/doing.php
+++
@@ -1,8 +0,0 @@
-{{ theme:partial name="doing_header" module="doing" }}
-<div id="content">		
-	{{ theme:partial name="doing_post" module="doing" }}	
-	{{ theme:partial name="doing_list" module="doing" }}
-</div>
-<div id="sidebar">
-	{{ theme:partial name="mood_list" module="doing" }}
-</div>
